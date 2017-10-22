155cfc0c4d8c14290f3b98be5617ff4d592ed38c
Kirill <kirill@Kirills-MacBook-Pro-2.local>
2017-10-22 14:41:07 -0700

    added comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,9 +2,13 @@ import media
 import fresh_tomatoes
 import imdb  # pip install imdb
 
+# initialize IMDB and get firs 6 movies
+# from the list of top 250 movies
 ia = imdb.IMDb()
-top10 = ia.get_top250_movies()[:6]
+top6 = ia.get_top250_movies()[:6]
 movies = []
+
+#trailers for those top 6 movies
 trailers = ['https://www.youtube.com/watch?v=6hB3S9bIaco',
             'https://www.youtube.com/watch?v=sY1S34973zA',
             'https://www.youtube.com/watch?v=8PyZCU2vpi8',
@@ -12,10 +16,15 @@ trailers = ['https://www.youtube.com/watch?v=6hB3S9bIaco',
             'https://www.youtube.com/watch?v=Dosg0p7LAB4',
             'https://www.youtube.com/watch?v=M5FpB6qDGAE']
 
-for index, movie in enumerate(top10):
+# iterate over movies and
+# create instances of class Movie
+for index, movie in enumerate(top6):
     ia.update(movie)
     movies.append(
-        media.Movie(movie['long imdb title'], movie['plot outline'],
-                    movie['full-size cover url'], trailers[index]))
+        media.Movie(movie['long imdb title'],
+                    movie['plot outline'],
+                    movie['full-size cover url'],
+                    trailers[index]))
 
+# pass movie array to fresh tomatoes library
 fresh_tomatoes.open_movies_page(movies)
